a5d4ad8e4d6dee9fe0835f3317b8162ddb194d07
satomi12120 <lance304307@gmail.com>
2025-06-06 17:56:37 +0800

    landing-page.php
    Updated landing-page.php as of 06/06/25
    
    - L

--- landing-page.php
+++ landing-page.php
@@ -5,21 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/finals/styles/main-style.css">
-    <title>Book flights with Pampanga Airlines</title>
+    <title>Book flights with Local Pampanga Airlines! </title>
 </head>
 <body>
     <header>
         <nav>
             <img src="" alt="Logo">
             <ul>
+                <li class="nav-links"><a href="dashboard.php">My Profile</a></li>
                 <li class="nav-links"><a href="book.php">Book</a></li>
-                <li class="nav-links"><a href="#">Reserve</a></li>
+                <li class="nav-links"><a href="reserve.php">Reserve</a></li>
                 <li class="nav-links"><a href="#">Explore</a></li>
                 <li class="nav-links"><a href="#">About</a></li>
                 <li class="nav-links"><a href="#">Contact</a></li>
             </ul>
             <button>
                 <a href="user-login.php">Log in</a>
+        </button>
+        <button>
+                <a href="user-login.php">Log Out</a>
         </button>
         </nav>
         <div class="center-text">
